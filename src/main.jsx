bfc7641af957bc19a3e8d9412731ce14432c4cde
SamG90 <112873929+SamG90@users.noreply.github.com>
2026-03-12 16:05:34 +1000

    feat: implement vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.jsx';
 import './index.css';
 
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ConvexProvider client={convex}>
       <App />
+      <Analytics />
     </ConvexProvider>
   </React.StrictMode>,
 );
